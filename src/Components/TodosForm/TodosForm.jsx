643f405a11ccf01cb9e3ff465228e50dbe129ca9
Shukhratovich-a <shukhratovich138@gmail.com>
2022-03-05 03:28:57 +0500

    Add buttons

--- src/Components/TodosForm/TodosForm.jsx
+++ src/Components/TodosForm/TodosForm.jsx
@@ -1,7 +1,9 @@
 function TodosForm() {
   return (
     <form className="todos__form">
-      <input className="todos__input" type="text" autoComplete="off" />
+      <button className="todos__button" name="check-all" type="button"></button>
+      <input className="todos__input" name="todo_text" type="text" autoComplete="off" />
+      <button className="todos__form__button" name="add-todo" type="submit"></button>
     </form>
   );
 }
